4b11dcfdda52cfd37e59379635981056de9dfa07
Ali Ash <AliAshooriyoon1@gmail.com>
2023-10-13 20:52:22 +0330

    Use testing drag & drop

--- src/script.js
+++ src/script.js
@@ -1,12 +1,22 @@
 let drag1 = document.getElementById("dragFile1");
 let drag2 = document.getElementById("dragFile2");
 let dropBox = document.getElementById("drop-Box");
-drag1.addEventListener("dragstart", draged);
-drag2.addEventListener("dragstart", draged);
-function draged() {
+let dragElement;
+drag1.addEventListener("dragstart", drag);
+drag2.addEventListener("dragstart", drag);
+function drag(event) {
   console.log("drag!");
+  event.dataTransfer.setData("elementID", event.target.id);
+  console.log(event.dataTransfer.getData("elementID"));
 }
 dropBox.addEventListener("dragover", function (event) {
   event.preventDefault();
-  console.log(event);
+  // console.log("dragOver");
+});
+dropBox.addEventListener("drop", function (event) {
+  console.log("drop!");
+  dragElement = document.getElementById(
+    event.dataTransfer.getData("elementID"),
+  );
+  event.target.append(dragElement);
 });
