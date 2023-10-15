eeb4159d94acdee55282695b235bb9791761f0c0
AliAshooriyoon <AliAshooriyoon1@gmail.com>
2023-10-15 22:20:07 +0330

    Added Contant Index to Script

---
+++ src/script.js
@@ -0,0 +1,5 @@
+const box1 = document.querySelector(".box1");
+const box2 = document.querySelector(".box2");
+const box3 = document.querySelector(".box3");
+const box4 = document.querySelector(".box4");
+const addToDoBtn = document.getElementById("addToDo");
